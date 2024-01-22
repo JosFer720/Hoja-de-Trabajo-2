23ada90b0bb0d0034fdda25a382aae2ef40ec411
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2024-01-22 17:31:05 -0600

    Creacion de metodos para interfaz

--- ICalculadora.java
+++ ICalculadora.java
@@ -1,3 +1,15 @@
-public interface ICalculadora {
+public interface ICalculadora<T> {
+
+int sumar();
+
+int restar();
+
+int multiplicar();
+
+double dividir();
+
+void push(T value);
+
+void pop(T value);
 
 }
