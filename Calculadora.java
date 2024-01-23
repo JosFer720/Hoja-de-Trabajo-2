89597df859d012e34751507a856304de7c814a05
erickguerra2 <eagi578@gmail.com>
2024-01-23 17:34:24 -0600

    Validacion divicion

--- Calculadora.java
+++ Calculadora.java
@@ -16,7 +16,13 @@ public class Calculadora implements ICalculadora {
     }
 
     public double dividir(int op1, int op2){
-        double resultado= op1 /  op2;
+        double resultado = 0;
+        if (op2 != 0){
+            resultado= op1 /  op2;
+        }
+        else{
+            System.out.println("Error: No se puede dividir entre cero");
+        }
         return resultado;
     }
 }
