6620c056aac08ee66aafc9c82d9bfe643ba035ff
erickguerra2 <eagi578@gmail.com>
2024-01-22 18:43:27 -0600

    Leer archivos 1

--- Principal.java
+++ Principal.java
@@ -1,3 +1,27 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Principal {
-    
+    BufferedReader br=null;
+    FileReader reader=null;
+    String line=null;
+    public void cargarArchivo(String archivo){
+        try{
+            File doc = new File(archivo);
+            reader = new FileReader(doc);
+            br= new BufferedReader(reader);
+            ArrayList<String> datos= new ArrayList<>();
+            while ((line=br.readLine())!= null){
+                datos.add(line.toString());
+            }
+            for (String num:datos){
+                System.out.println(num);
+            }
+    }
+    catch(Exception a){
+        throw new RuntimeException("Error");
+    }
+}
 }
