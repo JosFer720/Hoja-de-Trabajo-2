c3ed34e1b505b740cb217eece46dd635e089ec71
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2024-01-23 20:49:04 -0600

    Eliminacion de lineas no usadas

--- Pruebas.java
+++ Pruebas.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Stack;
